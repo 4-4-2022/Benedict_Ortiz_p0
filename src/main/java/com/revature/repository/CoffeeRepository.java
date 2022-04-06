577e10895c57d567a6ea380b82ba37267673f4f0
Benedict Ortiz <benortiz1391@gmail.com>
2022-04-06 13:57:41 -0400

    formatting

--- src/main/java/com/revature/repository/CoffeeRepository.java
+++ src/main/java/com/revature/repository/CoffeeRepository.java
@@ -5,10 +5,9 @@ import java.util.ArrayList;
 import com.revature.model.Coffee;
 
 public class CoffeeRepository {
-	
+
 	private static ArrayList<Coffee> coffeeList = new ArrayList<Coffee>();
-	
-	
+
 	public CoffeeRepository() {
 		super();
 		coffeeList.add(new Coffee("Caramel Latte", 15, "Ben's Cafe"));
@@ -16,10 +15,9 @@ public class CoffeeRepository {
 		coffeeList.add(new Coffee("Cappucino", 10, "Ben's Cafe"));
 		coffeeList.add(new Coffee("Black", 7, "Ben's Cafe"));
 	}
-	
+
 	public ArrayList<Coffee> getCoffeeList() {
 		return coffeeList;
 	}
-	
 
 }
