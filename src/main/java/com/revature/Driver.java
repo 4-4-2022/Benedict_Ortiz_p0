03745aaa9531bf2a1a1d8bd2de7e2520235d39a5
Benedict Ortiz <benortiz1391@gmail.com>
2022-04-06 13:09:27 -0400

    added driver, coffee model, AppUI code

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -1,9 +1,36 @@
 package com.revature;
 
+import java.util.Scanner;
+
+import com.revature.client.AppUI;
+
 public class Driver {
-	
+
 	public static void main(String[] args) {
-		
+
+		AppUI.welcomeMessage();
+
+		Scanner scanner = new Scanner(System.in);
+
+		boolean isUserHere = true;
+		while (isUserHere) {
+
+			int userAnswer = scanner.nextInt();
+
+			switch (userAnswer) {
+			case 1:
+				System.out.println("this is where the menu would be");
+
+				break;
+			case 2:
+				AppUI.historyOfCoffeeShop();
+				break;
+			case 3:
+				System.out.println("bye customer");
+				isUserHere = false;
+			}
+		}
+		scanner.close();
 	}
 
 }
--- src/main/java/com/revature/client/AppUI.java
+++ src/main/java/com/revature/client/AppUI.java
@@ -2,4 +2,16 @@ package com.revature.client;
 
 public class AppUI {
 
+	public static void welcomeMessage() {
+		System.out.println("Welcome to Ben's Coffee shop! Where we have coffee that would keep even the dead awake."
+				+ "\n 1. View the coffee menu" + "\n 2. View the history of Ben's Coffee shop."
+				+ "\n 3. Exit the coffee shop.");
+	}
+
+	public static void historyOfCoffeeShop() {
+		System.out.println(
+				"The history of the Coffee shop began in Seattle, Washington. \nStarted before Starbucks, their coffee has the tastiest"
+						+ " and most caffeine content allowed by FDA for human consumption. \nGuaranteed to keep you awake with just one sip.");
+	}
+
 }
