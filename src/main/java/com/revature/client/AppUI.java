0598e8a586aa4ff59f019a7bbd14b5ad2cf79b59
Benedict Ortiz <benortiz1391@gmail.com>
2022-04-06 15:21:10 -0400

    redoing a lot of this, read proj desc wrong, added employee class

--- src/main/java/com/revature/client/AppUI.java
+++ src/main/java/com/revature/client/AppUI.java
@@ -1,6 +1,12 @@
 package com.revature.client;
 
 public class AppUI {
+	
+	public static void loginPage() {
+		System.out.println("Welcome to Ben's Coffee shop! Where we have coffee that would keep even the dead awake."
+				+ "\n 1. View the coffee menu" + "\n 2. View the history of Ben's Coffee shop."
+				+ "\n 3. Exit the coffee shop.");
+	}
 
 	public static void welcomeMessage() {
 		System.out.println("Welcome to Ben's Coffee shop! Where we have coffee that would keep even the dead awake."
